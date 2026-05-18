e41769c6789e1c52a354d6a793656e704d7ce586
k0cko <konstantin.romov@gmail.com>
2026-05-18 13:28:13 +0300

    refactor: httpException to return json

--- app/src/EventListener/ExceptionListener.php
+++ app/src/EventListener/ExceptionListener.php
@@ -6,6 +6,7 @@ use App\Exception\DuplicateResourceException;
 use App\Exception\InvalidStatusException;
 use App\Exception\OldPasswordMismatchException;
 use App\Exception\ResourceInUseException;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
@@ -24,11 +25,17 @@ class ExceptionListener
                 InvalidStatusException::class => $this->getResponse($exception->getMessage(), JsonResponse::HTTP_BAD_REQUEST),
                 OldPasswordMismatchException::class => $this->getResponse($exception->getMessage(), JsonResponse::HTTP_BAD_REQUEST),
                 ResourceInUseException::class => $this->getResponse($exception->getMessage(), JsonResponse::HTTP_CONFLICT),
+                UniqueConstraintViolationException::class => $this->getResponse('A duplicate resource already exists.', JsonResponse::HTTP_CONFLICT),
                 default => $this->getResponse('Ooops, something went wrong.', JsonResponse::HTTP_INTERNAL_SERVER_ERROR),
             };
-            
-            $event->setResponse($response);
+        } else {
+            $response = $this->getResponse(
+                $exception->getMessage(),
+                $exception->getStatusCode()
+            );
         }
+
+        $event->setResponse($response);
     }
 
     private function getResponse(string $message, int $errorCode): JsonResponse
--- app/src/Exception/DuplicateResourceException.php
+++ app/src/Exception/DuplicateResourceException.php
@@ -13,5 +13,15 @@ final class DuplicateResourceException extends \RuntimeException
         return new self(
             sprintf('%s with %s "%s" already exists.', $entity, $field, $value)
         );
-    }    
+    }
+
+    public static function forDuplicateProduct(
+        string $product,
+        string $entity,
+    ): self
+    {
+        return new self(
+            sprintf('%s already exists in the %s', $product, $entity)
+        );
+    }
 }
