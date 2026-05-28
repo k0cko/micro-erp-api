0c25a8836b0745e9ddb3b423aaa4e32161061125
k0cko <konstantin.romov@gmail.com>
2026-05-28 09:13:15 +0300

    feat: wrap json response in 'data' envelope

---
+++ app/src/EventListener/KernelResponseListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+#[AsEventListener]
+class KernelResponseListener
+{
+    public function __invoke(ResponseEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+
+        if ($response->headers->get('Content-Type') !== 'application/json') {
+            return;
+        }
+
+        try {
+            $contentData = json_decode(json: $response->getContent(), associative: true, flags: JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            return;
+        }
+
+        $response->setContent(json_encode(['data' => $contentData]));
+    }
+}
